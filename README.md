e98f3c42a7563f11e9cbc947e71d672345f752e2
Roni Kaufman <48394336+ronikaufman@users.noreply.github.com>
2024-01-01 15:50:17 +0100

    base + jan01

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# genuary2024
- Daily creative coding sketches for Genuary 2024
+# Genuary 2024
+
+[Genuary](genuary.art) is a month of daily generative art prompts, happening every January.
+
+For this 2024 edition, I gave myself extra constraints: 
+- square format, the result is a still image (not an animation);
+- draw with only black, white or shades or gray;
+- the background is one plain color, evolving every day
+
+### JAN. 1
+> Particles, lots of them.
+
+<img src="./jan01-10/jan01/jan01_01.png" width="40%"/> <img src="./jan01-10/jan01/jan01_02.png" width="40%"/> <img src="./jan01-10/jan01/jan01_03.png" width="40%"/> <img src="./jan01-10/jan01/jan01_04.png" width="40%"/>
---
+++ jan01-10/jan01/sketch.js
@@ -0,0 +1,107 @@
+/*
+Genuary 2024
+JAN.1 "Particles, lots of them."
+By Roni Kaufman
+https://ronikaufman.github.io
+*/
+
+let gravity;
+let circles = [];
+let particles = [];
+
+function setup() {
+    createCanvas(500, 500);
+
+    gravity = createVector(0, 0.1);
+
+    for (let i = 0; i < 1000; i++) {
+        let x0 = random(10, width-10);
+        let y0 = random(10, height-10);
+
+        let rMax = min([x0, y0, width-x0, height-y0]);
+        for (let c of circles) {
+            let d = dist(x0, y0, c.x, c.y) - c.r;
+            if (d < rMax) {
+                rMax = d;
+            }
+        }
+        if (rMax < 20) continue;
+        if (rMax > 100) rMax = 80;
+        circles.push({
+            x: x0,
+            y: y0,
+            r: rMax
+        });
+
+        let n = sqrt(rMax)*30;
+        for (let i = 0; i < n; i++) {
+            let theta = TAU*i/n + random(TAU/(2*n));
+            let sp = random(2, 5);
+            let life = sqrt(random())*rMax/3;
+            particles.push(new Particle(x0, y0, theta, sp, life, rMax));
+        }
+    }    
+}
+
+function draw() {
+    background(rainbow(1/31));
+
+    noFill();
+    stroke(0, 200);
+    for (let p of particles) {    
+        strokeWeight(sqrt(p.rMax)/8);
+        p.drawHistory();
+    }
+
+    fill(255);
+    noStroke();
+    for (let p of particles) {
+        if (p.isAlive()) {
+            p.update();
+        } else {
+            p.drawPos();
+        }
+    }
+}
+
+function distSquared(x1, y1, x2, y2) {
+    return sq(x1-x2)+sq(y1-y2);
+}
+
+function Particle(x0, y0, theta, sp, life, rMax) {
+    this.x0 = x0;
+    this.y0 = y0;
+    this.pos = createVector(x0, y0);
+    this.vel = p5.Vector.fromAngle(theta, sp);
+    this.acc = createVector(0, 0);
+    this.life = life;
+    this.rMax = rMax;
+    this.diam = random(sqrt(rMax)/5, sqrt(rMax)/3);
+    this.posHistory = [createVector(x0, y0)];
+
+    this.update = function() {
+        this.acc = createVector(0, 0);
+        this.acc.add(gravity);
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.posHistory.push(createVector(this.pos.x, this.pos.y));
+        this.life--;
+    }
+
+    this.isAlive = function() {
+        if (this.life <= 0) return false;
+        return true;
+    }
+
+    this.drawPos = function() {
+        circle(this.pos.x, this.pos.y, this.diam);
+    }
+
+    this.drawHistory = function() {
+        beginShape();
+        for (let po of this.posHistory) {
+            vertex(po.x, po.y);
+        }
+        endShape();
+    }
+}
